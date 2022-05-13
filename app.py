a2c63e09cfe328769a4985fb2d529d102ff91bb6
cah-ext-gopinathmuthusamy <44694356+cah-ext-gopinathmuthusamy@users.noreply.github.com>
2022-05-13 16:19:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
+import json
 from flask import Flask
-from flask_restplus import Api, Resource,reqparse
-from flask_cors import CORS
-
-application = Flask(__name__)
-
-
-API_NAME = Api(application, version="1.0",
-               title="BioInformatics",
-               default="/",
-               default_label="Bioinformatics use cases",
-               description="Swagger Documentation",
-               strict_slashes=False)
-CORS(application)
-
-API_NAME_BI = API_NAME.namespace('BioInformatics ', description= 'End points of BioInformatics use cases')
-
-if __name__ == '__main__':
-
-	app.run(port=8000 ,host='0.0.0.0')
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return json.dumps({'name': 'alice',
+                       'email': 'alice@outlook.com'})
+app.run()
