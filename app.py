6be5627116d465b072e3f412265c6bfd629017b4
cah-ext-gopinathmuthusamy <44694356+cah-ext-gopinathmuthusamy@users.noreply.github.com>
2022-05-18 08:10:43 +0530

    Docs Added

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
-api = Api(app)
+
+app.config['SECRET_KEY'] = 'disable the web security'
+app.config['CORS_HEADERS'] = 'Content-Type'
+API_NAME = Api(app,
+               version="1.0",
+               title="vLife Demo App",
+               default=None,
+               default_label=None,
+               description="Swagger Documentation of Demo app",
+               strict_slashes=False)
+
 
 class Helloworld(Resource):
 
@@ -20,7 +30,9 @@ class Helloworld(Resource):
 
 		}
 
-api.add_resource(Helloworld, '/')
+API_NAME = API_NAME.namespace('vLifeHealthMonitor', description= "API's for health monitor dashboard")
+	
+API_NAME.add_resource(Helloworld, '/')
 
 if __name__ == '__main__':
 
